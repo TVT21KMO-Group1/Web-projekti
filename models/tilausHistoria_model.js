a43df00b6172d9de5bb77e89f5a50bc002508114
Mokko <Laukkanen.mikko@gmail.com>
2022-04-15 14:27:35 +0300

    testi5

--- models/tilausHistoria_model.js
+++ models/tilausHistoria_model.js
@@ -6,6 +6,10 @@ const tilausHistoria = {
   getAll: function(idKayttaja, callback) {
     return db.query('select tilaus.idtilaus, tilaus.aika, ravintola.nimi, ruoka.tuote, tilaus.summa from tilaus join tilatuttuotteet on tilaus.idtilaus = tilatuttuotteet.tilaus_idtilaus join ruoka on tilatuttuotteet.tuotteet = ruoka.idruoka join ravintola on tilaus.idravintola = ravintola.idravintola join kayttaja on tilaus.kayttaja_idkayttaja = kayttaja.idkayttaja where kayttaja.idKayttaja = ?',
     [idKayttaja], callback);
+  },
+  getRavintola: function(idRavintola, callback) {
+    return db.query('select tilaus.idtilaus, tilaus.aika, ravintola.nimi, ruoka.tuote, tilaus.summa from tilaus join tilatuttuotteet on tilaus.idtilaus = tilatuttuotteet.tilaus_idtilaus join ruoka on tilatuttuotteet.tuotteet = ruoka.idruoka join ravintola on tilaus.idravintola = ravintola.idravintola where ravintola.idravintola = ?',
+    [idRavintola], callback);
   }
 };
 
